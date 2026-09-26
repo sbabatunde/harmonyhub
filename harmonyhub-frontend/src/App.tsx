fd5b1c9e0b641463dbe9d77d07c2c0d0fd528e42
Salawu Babatunde Michael <salawubabatunde69@gmail.com>
2026-09-26 06:31:43 +0100

    App.tsx error fix

--- harmonyhub-frontend/src/App.tsx
+++ harmonyhub-frontend/src/App.tsx
@@ -6,7 +6,7 @@ import {
 } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useAuthStore } from "@/store/authStore";
-import { logger } from "@/utils/logger";
+// import { logger } from "@/utils/logger";
 import { useEffect, useState } from "react";
 
 // Layouts
